ca4fad796faea4ce425fcec5ca76db6608dae857
Thomas Gammelby <gammelby_thomas@hotmail.com>
2021-04-13 17:51:45 +0200

    Templates til home login register user

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,19 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { from } from 'rxjs';
+import { LoginpageComponent } from './loginpage/loginpage.component';
+import { HomepageComponent} from './homepage/homepage.component';
+import { UserpageComponent } from './userpage/userpage.component';
+import { RegisterpageComponent } from './registerpage/registerpage.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'login', component:LoginpageComponent},
+  {path:'home', component: HomepageComponent},
+  {path: 'user', component: UserpageComponent},
+  {path: 'register', component: RegisterpageComponent},
+  {path:'', redirectTo:'login', pathMatch: 'full'},
+  {path: '**', redirectTo: 'home', pathMatch: 'full'}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,26 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomepageComponent } from './homepage/homepage.component';
+import { LoginpageComponent } from './loginpage/loginpage.component';
+import { UserpageComponent } from './userpage/userpage.component';
+import { AdminpageComponent } from './adminpage/adminpage.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { RegisterpageComponent } from './registerpage/registerpage.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomepageComponent,
+    LoginpageComponent,
+    UserpageComponent,
+    AdminpageComponent,
+    RegisterpageComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    NgbModule
   ],
   providers: [],
   bootstrap: [AppComponent]
